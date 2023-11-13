090e1acad684fb72e1e486dee94c815c655851ab
YuTengjing <ytj2713151713@gmail.com>
2023-11-13 22:59:04 +0800

    Append Argument

---
+++ src/00191-medium-append-argument/solution.ts
@@ -0,0 +1,56 @@
+/*
+  191 - Append Argument
+  -------
+  by Maciej Sikora (@maciejsikora) #medium #arguments
+
+  ### Question
+
+  For given function type `Fn`, and any type `A` (any in this context means we don't restrict the type, and I don't have in mind any type 😉) create a generic type which will take `Fn` as the first argument, `A` as the second, and will produce function type `G` which will be the same as `Fn` but with appended argument `A` as a last one.
+
+  For example,
+
+  ```typescript
+  type Fn = (a: number, b: string) => number
+
+  type Result = AppendArgument<Fn, boolean>
+  // expected be (a: number, b: string, x: boolean) => number
+  ```
+
+  > This question is ported from the [original article](https://dev.to/macsikora/advanced-typescript-exercises-question-4-495c) by [@maciejsikora](https://github.com/maciejsikora)
+
+  > View on GitHub: https://tsch.js.org/191
+*/
+
+/* _____________ Your Code Here _____________ */
+
+/**
+ * 思路：类型空间也可以对数组类型进行 spread
+ */
+type AppendArgument<Fn extends (...args: any) => any, A> = Fn extends (
+    ...args: infer Args
+) => infer Ret
+    ? (...args: [...Args, A]) => Ret
+    : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>;
+type Result1 = (a: number, b: string, x: boolean) => number;
+
+type Case2 = AppendArgument<() => void, undefined>;
+type Result2 = (x: undefined) => void;
+
+type cases = [
+    Expect<Equal<Case1, Result1>>,
+    Expect<Equal<Case2, Result2>>,
+    // @ts-expect-error
+    AppendArgument<unknown, undefined>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/191/answer
+  > View solutions: https://tsch.js.org/191/solutions
+  > More Challenges: https://tsch.js.org
+*/
