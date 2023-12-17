9d1ec4af1173bab3227c5ac00e47837574fdd1b8
YuTengjing <ytj2713151713@gmail.com>
2023-12-17 11:59:51 +0800

    612 - KebabCase

---
+++ src/00612-medium-kebabcase/solution.ts
@@ -0,0 +1,61 @@
+/*
+  612 - KebabCase
+  -------
+  by Johnson Chu (@johnsoncodehk) #medium #template-literal
+
+  ### Question
+
+  Replace the `camelCase` or `PascalCase` string with `kebab-case`.
+
+  `FooBarBaz` -> `foo-bar-baz`
+
+  For example
+
+  ```ts
+  type FooBarBaz = KebabCase<"FooBarBaz">
+  const foobarbaz: FooBarBaz = "foo-bar-baz"
+
+  type DoNothing = KebabCase<"do-nothing">
+  const doNothing: DoNothing = "do-nothing"
+  ```
+
+  > View on GitHub: https://tsch.js.org/612
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type IsUpperCase<C extends string> = Lowercase<C> extends C ? false : true;
+
+type KebabCase<
+    S extends string,
+    IsFirst extends boolean = true,
+> = S extends `${infer First}${infer Rest}`
+    ? IsUpperCase<First> extends true
+        ? `${IsFirst extends true ? '' : '-'}${Lowercase<First>}${KebabCase<Rest, false>}`
+        : `${First}${KebabCase<Rest, false>}`
+    : '';
+
+type R = KebabCase<'foo-bar'>;
+//   ^?
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+    Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+    Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+    Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+    Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+    Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+    Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+    Expect<Equal<KebabCase<'-'>, '-'>>,
+    Expect<Equal<KebabCase<''>, ''>>,
+    Expect<Equal<KebabCase<'😎'>, '😎'>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/612/answer
+  > View solutions: https://tsch.js.org/612/solutions
+  > More Challenges: https://tsch.js.org
+*/
