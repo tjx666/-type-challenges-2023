f058ee1c76b398f91b40050577f8c7378d308f68
YuTengjing <ytj2713151713@gmail.com>
2024-01-22 12:48:33 +0800

    3 道 required 相关 hard

---
+++ src/00089-hard-required-keys/solution.ts
@@ -0,0 +1,49 @@
+/*
+  89 - Required Keys
+  -------
+  by yituan (@yi-tuan) #hard #utils
+
+  ### Question
+
+  Implement the advanced util type `RequiredKeys<T>`, which picks all the required keys into a union.
+
+  For example
+
+  ```ts
+  type Result = RequiredKeys<{ foo: number; bar?: string }>;
+  // expected to be “foo”
+  ```
+
+  > View on GitHub: https://tsch.js.org/89
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type RequiredKeys<T, RT extends T = Required<T>, E extends keyof T = keyof T> = E extends E
+    ? T[E] extends RT[E]
+        ? E
+        : never
+    : never;
+
+type X = Pick<{ name?: string }, 'name'>;
+type Y = {} extends { name: string } ? true : false;
+type Z = {} extends { name?: string } ? true : false;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+    Expect<Equal<RequiredKeys<{ a: number; b?: string }>, 'a'>>,
+    Expect<Equal<RequiredKeys<{ a: undefined; b?: undefined }>, 'a'>>,
+    Expect<
+        Equal<RequiredKeys<{ a: undefined; b?: undefined; c: string; d: null }>, 'a' | 'c' | 'd'>
+    >,
+    Expect<Equal<RequiredKeys<{}>, never>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/89/answer
+  > View solutions: https://tsch.js.org/89/solutions
+  > More Challenges: https://tsch.js.org
+*/
